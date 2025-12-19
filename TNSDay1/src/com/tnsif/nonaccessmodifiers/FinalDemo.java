3b8630e10ec2e74022eba5bd1acbeb4cea57a1af
Nanmaran19 <nanmaran2022@gmail.com>
2025-12-19 11:19:23 +0530

    Add Files

---
+++ TNSDay1/src/com/tnsif/nonaccessmodifiers/FinalDemo.java
@@ -0,0 +1,19 @@
+package com.tnsif.nonaccessmodifiers;
+
+final class FinalDemo {
+	final int Var = 100; 
+	final void display() {
+		System.out.println("Show the value of max:" + Max);
+	}
+	public static void main(String[] args) {
+		FinalDemo fd = new FinalDemo();
+		fd.display();
+		fd.Var = 100;
+	}
+}
+// Cannot extend the Final Class
+class Child extends FinalDemo {
+	void display() {
+		System.out.println("Show the Value of Max:" + Max);
+	}
+}
